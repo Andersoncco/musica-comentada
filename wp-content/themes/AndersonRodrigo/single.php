f10b9e6ce2fcecef00d35fe1104d767bf0bf2bbd
Anderson Rodrigo <anderson_lampugnani@hotmail.com>
2013-04-16 17:19:12 -0300

    adicionando tema andersonrodrigo

---
+++ wp-content/themes/AndersonRodrigo/single.php
@@ -0,0 +1,27 @@
+<?php get_header() ?>
+ 
+	<div class="content">
+ 
+		<section class="main">
+		<?php
+		while (have_posts()) {
+			the_post();
+		?>
+			<article id="post-<?php the_ID() ?>" <?php post_class() ?>>
+				<header>
+					<h1><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h1>
+					<time datetime="<?php the_time('c') ?>"><?php the_time(get_option('date_format')) ?></time>
+				</header>
+ 
+				<div>
+					<?php the_content() ?>
+				</div>
+			</article>
+		<?php } ?>
+		</section>
+ 
+		<?php get_sidebar() ?>
+		
+	</div>
+ 
+<?php get_footer() ?>
